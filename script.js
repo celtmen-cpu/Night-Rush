f4ee5c86d33fa109a88682ab1ba7d55d345e4aad
celtmen <achintoutain.clement@icloud.com>
2026-06-17 20:45:52 +0200

    Update script.js

--- script.js
+++ script.js
@@ -18,22 +18,22 @@ const links = {
 
     windows: {
         msg: "Download Windows",
-        url: "https://github.com/celtmen-cpu/Night-Rush/releases/download/0.30/windows-v0.30.exe"
+        url: "https://github.com/celtmen-cpu/Night-Rush/releases/download/0.31/windows-v0.31.exe"
     },
 
     mac: {
         msg: "Download Mac",
-        url: "https://github.com/celtmen-cpu/Night-Rush/releases/download/0.30/Ninja-Runner.mac-v0.30.dmg"
+        url: "https://github.com/celtmen-cpu/Night-Rush/releases/download/0.31/Ninja-Runner.mac-v0.31.dmg"
     },
 
     android: {
         msg: "Download Android",
-        url: "https://github.com/celtmen-cpu/Night-Rush/releases/download/0.30/android-v0.30.apk"
+        url: "https://github.com/celtmen-cpu/Night-Rush/releases/download/0.31/android-v0.31.apk"
     },
         
     iphone: {
         msg: "Download iphone",
-        url: "https://github.com/celtmen-cpu/Night-Rush/releases/download/0.30/NightRush.ipa"
+        url: "https://github.com/celtmen-cpu/Night-Rush/releases/download/0.31/NightRush.ipa"
     }
 
 };
